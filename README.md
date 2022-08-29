943d6099dc93288f8b207cc793c679cc147beef5
Jefferson Charlles <contato@jefferdeveloper.com>
2022-08-29 08:31:00 -0300

    features:integration hotmart 1.0 and fix uca

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
-# fullscreenjc
+# Fullscreenjc
+
+
+<div align="center">
+  <a href="https://opensource.org/licenses/MIT"><img alt="License MIT" src="https://img.shields.io/badge/license-MIT-brightgreen"></a>
+</div>
+
+<p align="center">
+  <a href="#interrobang-what-is-inter">About</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#technologies">Technologies used</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#construction_worker-how-to-use-developing">How to portfolio</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#key-license">License</a>
+</p>
+
+
+## PREVIEW
+<a href="https://chrome.google.com/webstore/detail/fullscreenjc/jgcackdhafaaedpmjnpnpmlnhoiplcgm?hl=pt-BR">Fullscreenjc</a>
+## 🚀 Technologies:
+
+This Chrome Extension project was developed using the following technologies:
+
+-   [Typescript][typescript]
+-   [Chrome Extension][extention]
+-   [Sass][sass]
+
+
+
+This project in under MIT license, for more details check in [LICENSE][license]. <br>
+Feel free to bring new features or fix problems, it will be a pleasure! 💜
+
+
+<div align='center'>
+  Made with 💚  by <strong>Jefferson Charlles</strong> 🔥
+  <a href='https://www.linkedin.com/in/jeffersoncharlles/'>Get in touch!</a>
+</div>
+
+[typescript]: https://www.typescriptlang.org/
+[extention]: https://chrome.google.com/webstore/
+[nextjs]: https://nextjs.org/
+[vitejs]: https://vitejs.dev/
+[styled]: https://styled-components.com/
+[phosphoricons]: https://phosphoricons.com/
+[react-hook-form]: https://react-hook-form.com/
+[sass]: https://sass-lang.com/
+[axios]: https://axios-http.com/docs/intro
+[prismic]: https://prismic.io/
+[stripe]: https://stripe.com/br
+[react-icons]: https://react-icons.github.io/react-icons/
+[git]: https://git-scm.com
+[fauna]: https://fauna.com/
+[yarn]: https://yarnpkg.com/
+[license]: ./LICENSE
+[linkedin]: https://www.linkedin.com/in/jeffersoncharlles/
+
+## 🔗 Links
+[![portfolio](https://img.shields.io/badge/my_portfolio-000?style=for-the-badge&logo=ko-fi&logoColor=white)](https://jefferdeveloper.com/)
+[![linkedin](https://img.shields.io/badge/linkedin-0A66C2?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/jeffersoncharlles)
--- src/script.js
+++ src/script.js
@@ -1,52 +1,96 @@
 
 
-const startApp = (contentMedia,wikiPageShow)=>{
+const startUCA = (contentMedia, wikiPageShow) => {
     const bgFull = document.createElement('div')
     bgFull.classList.add('bg-full')
 
 
     const btn = document.createElement('div')
     btn.classList.add('btn-fullscreen')
-    btn.innerText = 'Full'
+    btn.innerText = 'FULL'
 
     contentMedia.appendChild(btn)
 
     let isFullscreen = false;
 
     //quando clicar no botão
-    btn.onclick = function(){
+    btn.onclick = function () {
         if (!isFullscreen) {
             isFullscreen = true
             document.body.appendChild(bgFull)
-            //jogando no final do body um filho novo 
-
             bgFull.appendChild(contentMedia)
-           contentMedia.classList.add('full')
-        }else{
+            contentMedia.classList.add('full')
+            btn.innerText = 'Exit'
+        } else {
             isFullscreen = false
             contentMedia.classList.remove('full')
             wikiPageShow.appendChild(contentMedia)
-            bgFull.classList.remove('bg-full')
-            
-
+            bgFull.remove()
+            btn.innerText = 'FULL'
         }
     }
 
-    
+
 }
 
 
-const interval = setInterval(()=>{
+const startApp = (playerVideo, parentPlayerVideo) => {
+    const bgFullscreen = document.createElement('div')
+    bgFullscreen.classList.add('bg_full')
+
+
+    const btn = document.createElement('div')
+    btn.classList.add('btn-fullscreen')
+    btn.innerText = 'FULL'
+    playerVideo.appendChild(btn)
+
+    let isFullscreen = false
+
+
+    btn.onclick = () => {
+
+        if (!isFullscreen) {
+            isFullscreen = true
+            document.body.appendChild(bgFullscreen)
+            bgFullscreen.appendChild(playerVideo)
+            playerVideo.classList.add('fullscreenPage')
+            btn.innerText = 'Exit'
+        } else {
+            isFullscreen = false
+            playerVideo.classList.remove('fullscreenPage')
+            parentPlayerVideo.appendChild(playerVideo)
+            bgFullscreen.remove()
+            btn.innerText = 'FULL'
+        }
+
+
+
+    }
+
+}
+
+
+const interval = setInterval(() => {
+
+    //uca faculdade
     const contentMedia = document.querySelector('.show-content')
-    
+
+    // hotmart 1
+    const playerVideo = document.querySelector('.page__media')
+
 
     if (contentMedia) {
         const wikiPageShow = contentMedia.parentNode;
         clearInterval(interval)
-
-        startApp(contentMedia,wikiPageShow)
-        
+        startUCA(contentMedia, wikiPageShow)
     }
-},50)
+
+    if (playerVideo) {
+        const parentPlayerVideo = playerVideo.parentNode
+        clearInterval(interval)
+        startApp(playerVideo, parentPlayerVideo)
+    }
+
+}, 50)
 
 
